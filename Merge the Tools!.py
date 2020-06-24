e44027e5bbf6631ed1dee27f3c16bda630f498c5
Rishabh Gupta <44132253+rishabhgupta03@users.noreply.github.com>
2020-06-25 00:28:41 +0530

    Update Merge the Tools!.py
    corrected run time error

--- Merge the Tools!.py
+++ Merge the Tools!.py
@@ -1,13 +1,14 @@
 def merge_the_tools(string, k):
-    subsegment = len(string)/k
-    for i in range(0,len(string),int(subsegment)):
-        t=""
-        for j in range(i,k+i):
-            if string[j] in t:
+    subsegment = int(len(string)/k)
+    for i in range(subsegment):
+        t=string[i*k:(i+1)*k]
+        s=""
+        for j in t:
+            if j in s:
                 pass
             else:
-                t=t+string[j]
-        print(t)
+                s=s+j
+        print(s)
 
 if __name__ == '__main__':
     string, k = input(), int(input())
