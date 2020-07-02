b40fff38b75b30ea0631b216a622fffbeb46bf78
Rishabh Gupta <44132253+rishabhgupta03@users.noreply.github.com>
2020-07-02 23:19:34 +0530

    Create Mean,Var, and Std.py

---
+++ Mean,Var, and Std.py
@@ -0,0 +1,7 @@
+import numpy
+n,m = map(int, input().split())
+array = numpy.array([input().split() for _ in range(n)], int)
+numpy.set_printoptions(legacy='1.13')
+print(numpy.mean(array, axis = 1))
+print(numpy.var(array, axis = 0))
+print(numpy.std(array, axis = None))
